5d6a604a6ca036b7da064b5be83c32eb5f1d01f3
Unknown <matteo.furia@gmail.com>
2017-11-26 15:36:11 +0100

    Riorg codice
    Tutte le tabelle di import in un unico script

--- scripts/tables/I_allocation Qty.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE(
-	 Stylecolor         VARCHAR(50)
-	,Location           VARCHAR(50)
-	,Typology           VARCHAR(50)
-	,EventYear          VARCHAR(50)
-	,Allocation Qty     VARCHAR(50)
-
-)
