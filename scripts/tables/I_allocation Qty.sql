f235a37b3137e1d9605be9550daf07e378d81035
Unknown <matteo.furia@gmail.com>
2017-11-26 15:08:24 +0100

    First table

---
+++ scripts/tables/I_allocation Qty.sql
@@ -0,0 +1,8 @@
+CREATE TABLE(
+	 Stylecolor         VARCHAR(50)
+	,Location           VARCHAR(50)
+	,Typology           VARCHAR(50)
+	,EventYear          VARCHAR(50)
+	,Allocation Qty     VARCHAR(50)
+
+)
