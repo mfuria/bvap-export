c376700d221d92808153aed426524a31319e8fe1
Unknown <matteo.furia@gmail.com>
2017-11-30 16:46:48 +0100

    Preparate tutte le tabelle degli import (almeno quelli che mi interessano)

---
+++ scripts/queries/varie.sql
@@ -0,0 +1,68 @@
+SELECT 
+	i.Stylecolor
+	q.Location
+	qTypology
+	q.EventYear
+	Sum(q.[Allocation Qty]) AS [Allocation Qty]
+FROM [I_Allocation_Qty] q 
+JOIN [I_Tree Item] i
+ON q.sku = q.Item
+GROUP BY 
+	i.Stylecolor
+	q.Location
+	q.Typology
+	q.EventYear;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT 
+	MINI_TREE_location.Location,
+	MINI_TREE_location.[Location Desc],
+	MINI_TREE_location.Country,
+	MINI_TREE_location.[Country Desc],
+	MINI_TREE_location.Zone,
+	MINI_Tree_Item.Stylecolor,
+	MINI_Tree_Item.Style,
+	MINI_Tree_Item.Material,
+	MINI_Tree_Item.Color,
+	MINI_Tree_Item.Department,
+	[Allocation Qty STYCOL].[Allocation Qty],
+	[I_Allocation Val].[Allocation Val],
+	[I_Allocation Local Val].[Allocation Local Val]
+FROM 
+
+
+
+	(
+		[Allocation Qty STYCOL] INNER JOIN 
+		(
+			[I_Allocation Val] INNER JOIN 
+			(
+				[I_Allocation Local Val] INNER JOIN MINI_Tree_Item 
+				ON [I_Allocation Local Val].Stylecolor = MINI_Tree_Item.Stylecolor
+			) 
+		ON [I_Allocation Val].Stylecolor = MINI_Tree_Item.Stylecolor
+	) 
+	ON [Allocation Qty STYCOL].Stylecolor = MINI_Tree_Item.Stylecolor
+) 
+INNER JOIN MINI_TREE_location 
+ON (
+	[Allocation Qty STYCOL].Location = MINI_TREE_location.Location
+) AND (
+	[I_Allocation Val].Location = MINI_TREE_location.Location
+) AND (
+	[I_Allocation Local Val].Location = MINI_TREE_location.Location
+);
